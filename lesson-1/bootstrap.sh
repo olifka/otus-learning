bbfbc633db5741be553828ec013b4ec4add907b4
Aleksandr Oliferuk <olifka.kun@gmail.com>
2020-08-15 23:29:15 +0300

    Some result

--- lesson-1/bootstrap.sh
+++ lesson-1/bootstrap.sh
@@ -2,26 +2,31 @@
 echo -e "\nlsblk on start:\n"
 lsblk
 yum -y install mdadm
-mdadm --create /dev/md/raid0 --level=0 --raid-devices=2 /dev/sdb /dev/sdc
-mkfs.xfs /dev/md/raid0
-RAID_UUID=`blkid -o udev /dev/md/raid0|grep ID_FS_UUID=|awk -F "=" '{print $2}'`
-echo -e "\nRAID UUID = $RAID_UUID\n"
+mdadm --create /dev/md/raid1 --level=1 --raid-devices=2 /dev/sdb /dev/sdc
+mkfs.xfs /dev/md/raid1
+RAID_UUID=`blkid -o udev /dev/md/raid1|grep ID_FS_UUID=|awk -F "=" '{print $2}'`
 echo "UUID=$RAID_UUID						xfs	defaults	0 0" > /etc/fstab
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
+sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=1/g' /etc/default/grub
 echo -e "\nlsblk after RAID creation:\n"
 lsblk
-mount /dev/md/raid0 /mnt
+mount /dev/md/raid1 /mnt
 rsync -aAX --exclude={/dev/*,/mnt/*,/proc/*,/sys/*,/tmp/*,/run/*} / /mnt/
 mount --bind /sys /mnt/sys
 mount --bind /proc /mnt/proc
 mount --bind /dev /mnt/dev
-# chroot /mnt
-grub2-install /dev/sda
-sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=1/g' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-# exit 0
+echo '#!/bin/bash' > /mnt/grub_installer.sh
+echo 'grub2-install /dev/sda' >> /mnt/grub_installer.sh
+# echo 'sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=1/g' /etc/default/grub' >> /mnt/grub_installer.sh
+echo 'grub2-mkconfig -o /boot/grub2/grub.cfg' >> /mnt/grub_installer.sh
+# echo 'exit 0' >> /mnt/grub_installer.sh
+chmod +x /mnt/grub_installer.sh
+chroot /mnt ./grub_installer.sh
+rm -rf /mnt/grub_installer.sh
+# dd if=/dev/zero bs=1 count=1024 of=/dev/sda
 # umount /mnt/sys
 # umount /mnt/proc
 # umount /mnt/dev
 # umount /mnt
 reboot
+# exit 0
