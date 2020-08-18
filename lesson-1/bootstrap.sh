9e0f6edfefdee93f30b0a7a5e4b043f6c07afea2
Aleksandr Oliferuk <olifka.kun@gmail.com>
2020-08-18 18:07:08 +0300

    Trying to move root to new device

--- lesson-1/bootstrap.sh
+++ lesson-1/bootstrap.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 echo -e "\nlsblk on start:\n"
 lsblk
-yum -y install mdadm
-yes | mdadm --create /dev/md/raid1 --level=1 --raid-devices=2 /dev/sdb /dev/sdc
-mkfs.xfs /dev/md/raid1
-RAID_UUID=`blkid -o udev /dev/md/raid1|grep ID_FS_UUID=|awk -F "=" '{print $2}'`
+yum -y install mdadm xfsdump
+yes | mdadm --create /dev/md/raid0 --level=0 --raid-devices=2 /dev/sdb /dev/sdc
+parted -s -a optimal -- /dev/md/raid0 mkpart primary 1MiB -2048s
+mkfs.xfs /dev/md/raid0
+RAID_UUID=`blkid -o udev /dev/md/raid0|grep ID_FS_UUID=|awk -F "=" '{print $2}'`
 HDD_UUID=`blkid -o udev /dev/sda1|grep ID_FS_UUID=|awk -F "=" '{print $2}'`
-echo "UUID=$RAID_UUID						xfs	defaults	0 0" > /etc/fstab
-echo "/swapfile none swap defaults 0 0" >> /etc/fstab
-sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=1/g' /etc/default/grub
-sed -i "s/$HDD_UUID/$RAID_UUID/g" /boot/grub2/grub.cfg
 echo -e "\nlsblk after RAID creation:\n"
 lsblk
-mount /dev/md/raid1 /mnt
-rsync -aAX --exclude={/dev/*,/mnt/*,/proc/*,/sys/*,/tmp/*,/run/*} / /mnt/
-mount --bind /sys /mnt/sys
-mount --bind /proc /mnt/proc
-mount --bind /dev /mnt/dev
-grub2-install /dev/sda
-grub2-mkconfig -o /boot/grub2/grub.cfg
-# echo '#!/bin/bash' > /mnt/grub_installer.sh
-# echo 'grub2-install /dev/sda' >> /mnt/grub_installer.sh
-# echo 'sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=1/g' /etc/default/grub' >> /mnt/grub_installer.sh
-# echo 'grub2-mkconfig -o /boot/grub2/grub.cfg' >> /mnt/grub_installer.sh
-# echo 'exit 0' >> /mnt/grub_installer.sh
-# chmod +x /mnt/grub_installer.sh
-# chroot /mnt ./grub_installer.sh
-rm -rf /mnt/grub_installer.sh
-# dd if=/dev/zero bs=1 count=1024 of=/dev/sda
-umount -l /mnt/sys
-umount -l /mnt/proc
-umount -l /mnt/dev
-umount -l /mnt
+mount /dev/md/raid0 /mnt
+xfsdump -J - /dev/sda1 | xfsrestore -J - /mnt
+for i in /proc/ /sys/ /dev/ /run/ /boot/; do mount --bind $i /mnt/$i; done
+echo '#!/bin/bash' > /mnt/grub_installer.sh
+echo "RAID_UUID=$RAID_UUID" >> /mnt/grub_installer.sh
+echo "HDD_UUID=$HDD_UUID" >> /mnt/grub_installer.sh
+echo "grub2-mkconfig -o /boot/grub2/grub.cfg" >> /mnt/grub_installer.sh
+echo 'cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g; s/.img//g"` --force; done' >> /mnt/grub_installer.sh
+echo 'sed -i "s/$HDD_UUID/$RAID_UUID/g" /boot/grub2/grub.cfg' >> /mnt/grub_installer.sh
+chmod +x /mnt/grub_installer.sh
+chroot /mnt ./grub_installer.sh
 reboot
 # exit 0
