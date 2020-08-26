4e7dafd3d8c692e8df17532195542d2304b1ba4c
Aleksandr Oliferuk <olifka.kun@gmail.com>
2020-08-26 15:48:46 +0300

    Doing LVM lesson

---
+++ lesson-2/bootstrap-1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+yum -y install lvm2 xfsdump
+pvcreate /dev/sdb
+vgcreate vg_tmp_root /dev/sdb
+lvcreate -n lv_tmp_root -l +100%FREE /dev/vg_tmp_root
+mkfs.xfs /dev/vg_tmp_root/lv_tmp_root
+mount /dev/vg_tmp_root/lv_tmp_root /mnt
+xfsdump -J - /dev/VolGroup00/LogVol00 | xfsrestore -J - /mnt
+for i in /proc/ /sys/ /dev/ /run/ /boot/; do mount --bind $i /mnt/$i; done
+echo '#!/bin/bash' > /mnt/grub_installer.sh
+echo "grub2-mkconfig -o /boot/grub2/grub.cfg" >> /mnt/grub_installer.sh
+echo 'cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g; s/.img//g"` --force; done' >> /mnt/grub_installer.sh
+echo 'sed -i "s/lv=VolGroup00\/LogVol00/lv=vg_tmp_root\/lv_tmp_root/g" /boot/grub2/grub.cfg' >> /mnt/grub_installer.sh
+chmod +x /mnt/grub_installer.sh
+chroot /mnt ./grub_installer.sh
+echo '#!/bin/bash' > /mnt/grub_installer.sh
+echo "grub2-mkconfig -o /boot/grub2/grub.cfg" >> /mnt/grub_installer.sh
+echo 'cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g; s/.img//g"` --force; done' >> /mnt/grub_installer.sh
+echo 'sed -i "s/lv=vg_tmp_root\/lv_tmp_root/lv=VolGroup00\/LogVol00/g" /boot/grub2/grub.cfg' >> /mnt/grub_installer.sh
+echo 'sed -i "s/vg_tmp_root-lv_tmp_root/VolGroup00-LogVol00/g" /boot/grub2/grub.cfg' >> /mnt/grub_installer.sh
+chmod +x /mnt/grub_installer.sh
+echo '#!/bin/bash' > /mnt/etc/rc.d/rc.local
+echo "lvremove -f /dev/VolGroup00/LogVol00" >> /mnt/etc/rc.d/rc.local
+echo "yes | lvcreate -n LogVol00 -L 8G VolGroup00" >> /mnt/etc/rc.d/rc.local
+echo "mkfs.xfs /dev/VolGroup00/LogVol00" >> /mnt/etc/rc.d/rc.local
+echo "mount /dev/VolGroup00/LogVol00 /mnt" >> /mnt/etc/rc.d/rc.local
+echo "xfsdump -J - /dev/vg_tmp_root/lv_tmp_root | xfsrestore -J - /mnt" >> /mnt/etc/rc.d/rc.local
+echo "for i in /proc/ /sys/ /dev/ /run/ /boot/; do mount --bind $i /mnt/$i; done" >> /mnt/etc/rc.d/rc.local
+echo "chroot /mnt ./grub_installer.sh" >> /mnt/etc/rc.d/rc.local
+echo 'echo "" > /mnt/etc/rc.d/rc.local' >> /mnt/etc/rc.d/rc.local
+echo "reboot" >> /mnt/etc/rc.d/rc.local
+chmod +x /mnt/etc/rc.d/rc.local
+reboot
