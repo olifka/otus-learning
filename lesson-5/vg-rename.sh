52860293c8894b89e6da834691fc596414c3a126
olifka <olifka.kun@gmail.com>
2020-09-02 15:12:55 +0300

    Update vg-rename.sh

--- lesson-5/vg-rename.sh
+++ lesson-5/vg-rename.sh
@@ -8,7 +8,7 @@ sed -i 's/VolGroup00/VG00/g' /boot/grub2/grub.cfg
 # Генерируем новый initrd командой mkinitrd
 mkinitrd -f -v /boot/initramfs-$(uname -r).img $(uname -r)
 # Безуспешно пытаемся ребутнуться =)
-# Попробовал разные способы - в VirtualBox  один не сработал
+# Попробовал разные способы - в VirtualBox ни один не сработал
 init 6
 # systemctl reboot
 # reboot
