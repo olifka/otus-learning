edc570a6f7f770f28d521c8e7cb80e1529dd376e
Aleksandr Oliferuk <aleksandr.oliferuk@gs-labs.ru>
2020-09-09 18:23:37 +0300

    Arrrrr!

--- lesson-6/parser.sh
+++ lesson-6/parser.sh
@@ -5,14 +5,13 @@ YEAR_TIME=(`awk -F " - - " '{print $2}' access-4560-644067.log|awk '{print $1}'|
 DAY_MONTH=(`awk -F " - - " '{print $2}' access-4560-644067.log|awk '{print $1}'|awk -F "/" '{print $1 " " $2 " " $3}'|awk '{print $1 " " $2}'|sed -e 's/\[//g'|sed -e 's/ /-/g'|sed -e 's/Aug/08/g'`)
 IP_ADDRESSES=(`awk '{print $1}' access-4560-644067.log`)
 REQUEST_TYPES=(`awk '{print $6}' access-4560-644067.log|sed -e 's/\"//g'`)
-#echo ${REQUEST_TYPES[*]}
-COUNTER=${#YEAR_TIME[*]}
+# echo ${REQUEST_TYPES[*]}
 
-
-#while [ "$i" -lt "$COUNTER" ]
-#do
-#        #echo $i
-#        #echo $COUNTER
+# COUNTER=${#YEAR_TIME[*]}
+# while [ "$i" -lt "$COUNTER" ]
+# do
+#        # echo $i
+#        # echo $COUNTER
 #        YEAR=(`echo ${YEAR_TIME[$i]}|awk -F ":" '{print $1}'`)
 #        MONTH=(`echo ${DAY_MONTH[$i]}|awk -F "-" '{print $2}'`)
 #        DAY=(`echo ${DAY_MONTH[$i]}|awk -F "-" '{print $1}'`)
@@ -21,20 +20,26 @@ COUNTER=${#YEAR_TIME[*]}
 #        SECOND=(`echo ${YEAR_TIME[$i]}|awk -F ":" '{print $4}'`)
 #        TIMESTRING="$YEAR-$MONTH-$DAY $HOUR:$MINUTE:$SECOND EDT"
 #        TIMESTAMPS[$i]=`date -d "$TIMESTRING" +%s`
-#        #echo ${TIMESTAMPS[$i]}
-#        #echo ${TIMESTRING}
+#        # echo ${TIMESTAMPS[$i]}
+#        # echo ${TIMESTRING}
 #        (( i++ ))
-#done
+# done
 
-#echo ${#TIMESTAMPS[*]}
-IP_COUNTER=10
-ZERO=0
-while [ "$IP_COUNTER" -ge "$ZERO" ]
+# echo ${#TIMESTAMPS[*]}
+# echo ${IP_ADDRESSES[*]}
+
+TOP_IP=`(IFS=$'\n'; sort <<< "${IP_ADDRESSES[*]}") | uniq -c | awk '{print $1 " " $2}' | sort -nr`
+# echo $TOP_IP|awk '{print $1 " " $2}'
+
+COUNTER=10
+i=0
+j=1
+while [ "$i" -ne "$COUNTER" ]
 do
-	TOP_IP=`(IFS=$'\n'; sort <<< "${IP_ADDRESSES[*]}") | uniq -c | awk '{print $1}' | sort -nr`
-	echo ${TOP_IP[*]}
-	(( IP_COUNTER-- ))
+	echo "${TOP_IP[$i]} ${TOP_IP[$j]}"
+	(( i++ ))
+	(( j++ ))
+	echo $i
 done
 
 exit 0
-
