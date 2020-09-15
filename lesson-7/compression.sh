36e586cbe440030500bf9ba137f97c852a462099
Aleksandr Oliferuk <olifka.kun@gmail.com>
2020-09-16 00:24:57 +0300

    update lesson 7-2

--- lesson-7/compression.sh
+++ lesson-7/compression.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-calculate_zfs_space {
+function calculate_zfs_space {
     sleep 3
     du -hs -B1 /mnt/gzip9/ > file_size
     du -hs -B1 /mnt/lz4/ >> file_size
@@ -50,34 +50,48 @@ zfs set compress=zle zle/data
 zfs set dedup=on dedup/data
 
 cd /tmp
-echo -e "\nDownloading sample file (the 'World & Peace')...\n"
+echo -e "\rDownloading sample file (the 'World & Peace')..."
 wget -q -O War_and_Peace.txt http://www.gutenberg.org/ebooks/2600.txt.utf-8
-echo -e "\nCopying sample file to zfs-directories...\n"
+echo -e "\rCopying sample file to zfs-directories..."
 xargs -n 1 cp War_and_Peace.txt<<<"/mnt/gzip9/ /mnt/lz4/ /mnt/lzjb/ /mnt/zle/ /mnt/dedup/"
-echo -e "\nCalculating located space...\n"
+echo -e "\rCalculating located space..."
 calculate_zfs_space
-echo -e "\nCopying /usr/share/man to zfs-directories...\n"
+echo -e "\rCopying /usr/share/man to zfs-directories..."
 xargs -n 1 cp -r /usr/share/man<<<"/mnt/gzip9/ /mnt/lz4/ /mnt/lzjb/ /mnt/zle/ /mnt/dedup/"
-echo -e "\nCalculating located space...\n"
+echo -e "\rCalculating located space..."
 calculate_zfs_space
-echo -e "\nCopying /usr/bin to zfs-directories...\n"
+echo -e "\rCopying /usr/bin to zfs-directories..."
 xargs -n 1 cp -r /usr/bin<<<"/mnt/gzip9/ /mnt/lz4/ /mnt/lzjb/ /mnt/zle/ /mnt/dedup/"
-echo -e "\nCalculating located space...\n"
+echo -e "\rCalculating located space..."
 calculate_zfs_space
 
   # # #  # # #
  #  Task 2  #
 # # #  # # #
-echo "\Creating 10Mb dummy filled with zeros...\n"
-dd if=/dev/zero of=/tmp/0dummy -bs=1 -count=10000000
-echo -e "\nCopying 0dummy to zfs-directories...\n"
+echo -e "\rCreating 10Mb dummy filled with zeros..."
+dd if=/dev/zero of=/tmp/0dummy bs=1 count=10000000
+du -hs /tmp/0dummy
+echo -e "\rCopying 0dummy to zfs-directories..."
 xargs -n 1 cp /tmp/0dummy<<<"/mnt/gzip9/ /mnt/lz4/ /mnt/lzjb/ /mnt/zle/ /mnt/dedup/"
-echo -e "\nCalculating located space...\n"
+echo -e "\rCalculating located space..."
+calculate_zfs_space
+echo -e "\rCopying 0dummy to zfs-directories by 5 times..."
+touch /mnt/gzip9/0dummy0
+touch /mnt/lz4/0dummy1
+touch /mnt/lzjb/0dummy2
+touch /mnt/zle/0dummy3
+touch /mnt/dedup/0dummy4
+xargs -n 1 cp /tmp/0dummy<<<"/mnt/gzip9/0dummy0 /mnt/lz4/0dummy0 /mnt/lzjb/0dummy0 /mnt/zle/0dummy0 /mnt/dedup/0dummy0"
+xargs -n 1 cp /tmp/0dummy<<<"/mnt/gzip9/0dummy1 /mnt/lz4/0dummy1 /mnt/lzjb/0dummy1 /mnt/zle/0dummy1 /mnt/dedup/0dummy1"
+xargs -n 1 cp /tmp/0dummy<<<"/mnt/gzip9/0dummy2 /mnt/lz4/0dummy2 /mnt/lzjb/0dummy2 /mnt/zle/0dummy2 /mnt/dedup/0dummy2"
+xargs -n 1 cp /tmp/0dummy<<<"/mnt/gzip9/0dummy3 /mnt/lz4/0dummy3 /mnt/lzjb/0dummy3 /mnt/zle/0dummy3 /mnt/dedup/0dummy3"
+xargs -n 1 cp /tmp/0dummy<<<"/mnt/gzip9/0dummy4 /mnt/lz4/0dummy4 /mnt/lzjb/0dummy4 /mnt/zle/0dummy4 /mnt/dedup/0dummy4"
+echo -e "\rCalculating located space..."
 calculate_zfs_space
 
-echo -e "\nDownloading archive with test zpool...\n"
+echo -e "\rDownloading archive with test zpool..."
 wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KRBNW33QWqbvbVHa3hLJivOAt60yukkg' -O zfs_task1.tar.gz
-echo -e "\nUnarchiving...\n"
+echo -e "\rUnarchiving..."
 tar xf zfs_task1.tar.gz
 zpool import -d zpoolexport otus
 zpool get size otus
