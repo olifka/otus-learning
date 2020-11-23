c0dc83dafe6578eb18488797184384eb62713bea
Aleksandr Oliferuk <olifka.kun@gmail.com>
2020-11-23 18:25:08 +0300

    Lesson 8: comments and readmwe was added

--- lesson-8/ps-ax.py
+++ lesson-8/ps-ax.py
@@ -4,6 +4,10 @@ from re import findall
 from io import open
 
 
+# Функция get_pids().
+# Получает список номеров запущенных процессов (PIDs) путём анализа папки /proc -
+# забираем имена каталогов, которые состоят только из чисел.
+# Возвращает список PID'ов.
 def get_pids():
     dirnames= []
     pids_list = []
@@ -19,8 +23,15 @@ def get_pids():
 
     return pids_list
 
+
+# Список номеров процессов, запущенных в данный момент
 pids_list = get_pids()
 
+
+# Функция get_cmd().
+# Принимает на вход номер процесса (PID).
+# Путём чтения из файла /proc/PID/cmdline получает значение команды-запуска заданного PID'а
+# и возвращает её в виде строки.
 def get_cmd(pid):
     path = "/proc/{}/cmdline".format(pid)
     f = open(path, 'r')
@@ -30,6 +41,12 @@ def get_cmd(pid):
     return cmd
 
 
+# Функция get_time().
+# Принимает на вход номер процесса (PID).
+# Ихсодя из того, что папка-процесс создаётся в /proc одновременно с возникновением процесса
+# за счёт даты создания этой папки получаем время жизни процесса:
+# 'текущее значение времени' - 'время создания папки' ("ls -ld /proc/PID").
+# Возвращает строку формата 'ЧЧ:ММ'.
 def get_time(pid):
     now = datetime.now()
     current_year = now.year
@@ -45,6 +62,12 @@ def get_time(pid):
     return delta.strftime("%H:%M")
 
 
+# Функция get_tty().
+# Принимает на вход номер процесса (PID).
+# Решение честно спёр со StackOverflow:
+# если на файл "/proc/PID/fd/0" сделать "ls -l|awk '{ print $11 }'",
+# то можно получить tty, используемый процессом.
+# Возвращает название tty.
 def get_tty(pid):
     path = "/proc/{}/fd/0".format(pid)
     bashCmd = "/bin/ls -l {}|awk \'{{ print $11 }}\'".format(path)
@@ -53,6 +76,10 @@ def get_tty(pid):
     return tty.rstrip('\n').lstrip('/dev/')
 
 
+# Функция get_stat().
+# Принимает на вход номер процесса (PID).
+# Из файла "/proc/PID/status" grep'аем строку State,
+# возвращаем её значение.
 def get_stat(pid):
     path = "/proc/{}/status".format(pid)
     bashCmd = "cat {} |grep State|awk \'{{ print $2 }}\'".format(path)
@@ -61,8 +88,10 @@ def get_stat(pid):
     return state.rstrip('\n')
 
 
+# Пародируем вывод нативного 'ps ax'
 print("PID TTY STAT TIME COMMAND")
 
+# Построчно выводим параметры запущенных процессов
 for pid in pids_list:
     tty = get_tty(pid)
     state = get_stat(pid)
